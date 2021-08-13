48d3b9356c57ea17e5a546e3dddeb46548bffac6
Daniel <narselldev@gmail.com>
2021-08-13 17:37:27 -0500

    Initial files and database connection

---
+++ src/test/java/misiontic/microservicios/pagos/pagos_ms/PagosMsApplicationTests.java
@@ -0,0 +1,13 @@
+package misiontic.microservicios.pagos.pagos_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
